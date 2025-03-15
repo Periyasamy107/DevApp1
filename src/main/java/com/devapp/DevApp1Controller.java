0a39de148b854cd5336bc886bde4fba3d2b3ebc8
Periyasamy <cperiyasamy085@gmail.com>
2025-03-15 23:08:04 +0530

    hello above universe

--- src/main/java/com/devapp/DevApp1Controller.java
+++ src/main/java/com/devapp/DevApp1Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevApp1Controller {
 	@GetMapping(value = "/name")
 	public String getName() {
-		return "Hello Universe";
+		return "Hello Above universe";
 	}
 }
