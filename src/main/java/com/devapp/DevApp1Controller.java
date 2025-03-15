9d7d18ceb3d26c1ccfdd696f26cd6e745f5a68d8
Periyasamy <cperiyasamy085@gmail.com>
2025-03-15 22:57:35 +0530

    updated the jar

--- src/main/java/com/devapp/DevApp1Controller.java
+++ src/main/java/com/devapp/DevApp1Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DevApp1Controller {
 	@GetMapping(value = "/name")
 	public String getName() {
-		return "My Name is Biggod";
+		return "This endpoint is changed for testing purpose";
 	}
 }
